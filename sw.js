a0f6e9251934d958089a2e0e8d8471901e140704
llorente21 <llorente21@gmail.com>
2026-08-24 00:20:53 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // MANGA Service Worker - NETWORK ONLY para HTML (siempre fresco)
-const CACHE = 'manga-v3.5';
+const CACHE = 'manga-v3.7';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
